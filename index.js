3fc1714fd186275a4738286dbf5edf08b67c70c6
nnnammmz <jieun@wemarkup.com>
2026-03-24 16:39:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //
 
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
 const ANTHROPIC_KEY = process.env.ANTHROPIC_API_KEY;
